c34eafd12218391c3e2006fb0c5040d605cd50c7
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-04-21 21:11:02 +0700

    radio button va radio group

--- app/src/main/java/com/example/drawableresource25022020/MainActivity.java
+++ app/src/main/java/com/example/drawableresource25022020/MainActivity.java
@@ -5,24 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    CheckBox mCbAndroid;
+//    CheckBox mCbAndroid;
+    RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mCbAndroid = findViewById(R.id.checkbox);
-        mCbAndroid.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//        mCbAndroid = findViewById(R.id.checkbox);
+//        mCbAndroid.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                if (b){
+//                    Toast.makeText(MainActivity.this, "Đã check", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Toast.makeText(MainActivity.this, "Bỏ check", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b){
-                    Toast.makeText(MainActivity.this, "Đã check", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(MainActivity.this, "Bỏ check", Toast.LENGTH_SHORT).show();
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.radioAndroid :
+                        Toast.makeText(MainActivity.this, "Android", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioJavascript :
+                        Toast.makeText(MainActivity.this, "javascript", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioReacttnatie :
+                        Toast.makeText(MainActivity.this, "react native", Toast.LENGTH_SHORT).show();
+                        break;
                 }
             }
         });
